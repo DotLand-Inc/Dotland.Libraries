918a83a29a34996e6c01848db03d63711c981f5a
Hamza SAII <hamza.saii@outlook.com>
2023-07-27 11:39:25 +0200

    add http abstraction project

---
+++ src/Dotland.Http.Abstractions/Models/Enums/MetaStatusType.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Dotland.Http.Abstractions.Models.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum MetaStatusType
+{
+    /// <summary>
+    /// Enum SucceededEnum for succeeded
+    /// </summary>
+    [EnumMember(Value = "succeeded")]
+    Succeeded = 0,
+
+    /// <summary>
+    /// Enum PartiallySucceededEnum for partially_succeeded
+    /// </summary>
+    [EnumMember(Value = "partially_succeeded")]
+    PartiallySucceeded = 1,
+
+    /// <summary>
+    /// Enum FailedEnum for failed
+    /// </summary>
+    [EnumMember(Value = "failed")]
+    Failed = 2,
+}
---
+++ src/Dotland.Http.Abstractions/UseCases/IResponseMessageFactory.cs
@@ -0,0 +1,17 @@
+﻿using Dotland.Http.Abstractions.Models.Responses;
+
+namespace Dotland.Http.Abstractions.UseCases;
+
+public interface IResponseMessageFactory
+{
+    Response Success();
+    Response Failure(string errorCode, string errorMessage, string? supportId);
+    Response Failure(List<(string errorCode, string errorMessage, string? supportId)> errors);
+    Response Failure(List<Message> errorMessages);
+}
+
+public interface IResponseMessageFactory<T> : IResponseMessageFactory
+    where T : class, new()
+{
+    Response<T> Success(T messages);
+}
